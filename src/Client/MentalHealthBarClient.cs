0c0264a7f4c65c70aa0879c2ae94f6ed3f679c37
Daryl Walleck <dwalleck@gmail.com>
2020-06-19 12:00:58 -0500

    Pulled over work for activities page

---
+++ src/Client/MentalHealthBarClient.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using MentalHealthBar.Client.Models;
+
+namespace MentalHealthBar.Client
+{
+    public class MentalHealthBarClient
+    {
+        private readonly HttpClient _client;
+
+        public MentalHealthBarClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<ActivityDto>> GetActvities()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/activities");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var response = await _client.SendAsync(request);
+            var activities = await response.Content.ReadFromJsonAsync<List<ActivityDto>>();
+            response.EnsureSuccessStatusCode();
+            return activities;
+        }
+    }
+}
